12fa54e9ee28e29c160e6f1531f68721a7d97722
Florian Taut <f@flrn.me>
2018-05-16 13:24:45 +0200

    (eslint): added eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var fs = require('fs');
+import * as fs from 'fs';
+import * as http from 'http';
 
 let jdata = 0;
 let jsonstring;
@@ -13,7 +13,7 @@ console.log('Server is listening on 3000');
 
 // Reading Jobs from file
 fs.readFile('res/test', 'utf8', (err, data) => {
-    if(err) throw err;
+    if (err) throw err;
     jdata = data.toString().split(',');
     jsonstring = JSON.stringify(jdata).split("\\n");
     fs.writeFile('res/data.json', jsonstring);
