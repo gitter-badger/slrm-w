af2a87e664614730e1245960962a9c914bafb3a3
Florian Taut <f@flrn.me>
2018-06-04 16:26:49 +0200

    refresh page every 15 min
    work for #13

--- view/js/controller.js
+++ view/js/controller.js
@@ -6,3 +6,29 @@ angular.module('slrm').controller('slrmctrl', ['$scope', '$http', function ($sco
             $scope.data = response.data;
         });
 }]);
+
+/*
+angular.module('slrm').controller('slrmctrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+    $scope.refreshInterval = 900; // Sec (15 Minutes)
+
+    $scope.getData = function () {
+        $http.get('/out/data.json')
+            .then(function(response) {
+                $scope.data = response.data;
+
+                return $timeout($scope.getData, $scope.getTimeLeftToRefresh());
+            });
+    };
+
+    $scope.getTimeLeftToRefresh = function () {
+        var now = new Date();
+        var timeLeftToRefresh = ($scope.refreshInterval - (now.getMinutes() * 60 + now.getSeconds()) % $scope.refreshInterval) * 1000;
+
+        return timeLeftToRefresh;
+    };
+
+    // Initial call to start the loop
+    $timeout($scope.getData, $scope.getTimeLeftToRefresh());
+
+}]);
+*/
